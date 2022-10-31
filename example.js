1b1f2e12d049e737c7b467407cd7473f888f1b63
Jacopo Marrone <jacopo.marrone27@gmail.com>
2022-11-01 00:42:07 +0100

    refactor: use yarn workspaces and ts

--- example.js
+++
@@ -1,9 +0,0 @@
-const { processDataSet } = require("./script");
-
-const DATASET1 = [
-  ["Mario", "Luigi", "Silvio"],
-  ["gioca a pallone", "scrive una lettera", "salta la corda"],
-  ["in cortile", "al bar", "in camera"],
-];
-
-processDataSet("esempi-azioni", DATASET1);
